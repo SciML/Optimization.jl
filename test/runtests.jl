d71aa75238f4bc048233eb1064ac09d121a10e4a
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2020-11-02 11:19:58 +0530

    Add println heading for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using SafeTestsets
 
+println("Rosenbrock Tests")
 @safetestset "Rosenbrock" begin include("rosenbrock.jl") end
+println("AD Tests")
 @safetestset "AD Tests" begin include("ADtests.jl") end
+println("Mini batching Tests")
 @safetestset "Mini batching" begin include("minibatch.jl") end
