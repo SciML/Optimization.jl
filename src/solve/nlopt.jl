d2367ff582dbb30ce6ca3634e9490224bf30241b
Jonas Mackerodt <jonas.mackerodt16@imperial.ac.uk>
2021-09-08 23:02:17 +0100

    Made NLopt.jl interface properly handle options & minor changes to MOI
    NLopt.jl interface now takes option for global optimiser as kwargs and if a `local_method` is defined  maxiters and option for the local optimiser can be passed as `local_maxiters` and a NamedTuple to `local_options` respectively.
    
    Moreover, the interface now takes both `NLopt.Opt` and the newly defined `NLO` type as optimiser input. The `NLO` type follows the same logic of `BBO`. 
    
    Also I removed n_starts argument as it was passed to the optimiser and only used to multiply the `maxiters`. 
    
    The NLopt `__solve` also passes `ret` to the `retcode` of `SciMLBase.SciMLBase.OptimizationSolution` and the NLopt model is passed to `original` field.
    
    Additionally, for `MOI` interface now returns the `MOI.TerminationStatus()` via the `retcode` field and the optimised `MOI` problem via the `original` field of `SciMLBase.SciMLBase.OptimizationSolution`.
    
    I have added some test for the changed interface

--- src/solve/nlopt.jl
+++ src/solve/nlopt.jl
@@ -1,6 +1,15 @@
-function __solve(prob::OptimizationProblem, opt::NLopt.Opt;
-                 maxiters = nothing, nstart = 1,
-                 local_method = nothing,
+export NLO
+
+struct NLO
+    method::Symbol
+    NLO(method) = new(method)
+end
+
+function __solve(prob::OptimizationProblem, opt::Union{NLO, NLopt.Opt};
+                 maxiters = nothing,
+                 local_method::Union{NLO, NLopt.Opt, Nothing} = nothing,
+                 local_maxiters = nothing,
+                 local_options::Union{NamedTuple,Nothing} = nothing,
                  progress = false, kwargs...)
     local x
 
@@ -25,27 +34,61 @@ function __solve(prob::OptimizationProblem, opt::NLopt.Opt;
         return _loss(θ)
     end
 
-    NLopt.min_objective!(opt, fg!)
+    if isa(opt,NLopt.Opt)
+        if ndims(opt) != length(prob.u0)
+            error("Passed NLopt.Opt optimization dimension does not match OptimizationProblem dimension.")
+        end
+        model = opt
+    else
+        model = NLopt.Opt(opt.method, length(prob.u0))
+    end
+
+    NLopt.min_objective!(model, fg!)
 
     if prob.ub !== nothing
-        NLopt.upper_bounds!(opt, prob.ub)
+        NLopt.upper_bounds!(model, prob.ub)
     end
+
     if prob.lb !== nothing
-        NLopt.lower_bounds!(opt, prob.lb)
+        NLopt.lower_bounds!(model, prob.lb)
     end
+
     if !(isnothing(maxiters))
-        NLopt.maxeval!(opt, maxiters)
+        NLopt.maxeval!(model, maxiters)
     end
-    if nstart > 1 && local_method !== nothing
-        NLopt.local_optimizer!(opt, local_method)
-        if !(isnothing(maxiters))
-            NLopt.maxeval!(opt, nstart * maxiters)
+
+    if local_method !== nothing
+        if isa(local_method,NLopt.Opt)
+            if ndims(local_method) != length(prob.u0)
+                error("Passed local NLopt.Opt optimization dimension does not match OptimizationProblem dimension.")
+            end
+        else
+            local_method = NLopt.Opt(local_method.method, length(prob.u0))
         end
+
+        if !(isnothing(local_maxiters))
+            NLopt.maxeval!(local_method, local_maxiters)
+        end
+
+        if !isnothing(local_options)
+            for j in Dict(pairs(local_options))
+                eval(Meta.parse("NLopt."*string(j.first)*"!"))(local_method, j.second)
+            end
+        end
+
+        NLopt.local_optimizer!(model, local_method)
     end
+
+    # add optimiser options from kwargs
+    for j in kwargs
+        eval(Meta.parse("NLopt."*string(j.first)*"!"))(model, j.second)
+    end
+
+    # print(model)
 
     t0 = time()
-    (minf,minx,ret) = NLopt.optimize(opt, prob.u0)
+    (minf,minx,ret) = NLopt.optimize(model, prob.u0)
     _time = time()
 
-    SciMLBase.build_solution(prob, opt, minx, minf; original=nothing)
+    SciMLBase.build_solution(prob, opt, minx, minf; original=model, retcode=ret)
 end
