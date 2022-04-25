d87e4b0026bc4965f13c7c80c1adaf7e4f70f340
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-04-25 07:20:09 -0400

    clean up test script

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,33 @@
 using SafeTestsets, Pkg
 
 const GROUP = get(ENV, "GROUP", "All")
-const is_APPVEYOR = Sys.iswindows() && haskey(ENV,"APPVEYOR")
-const is_TRAVIS = haskey(ENV,"TRAVIS")
 
-function activate_downstream_env()
-    Pkg.activate("downstream")
-    Pkg.develop(PackageSpec(path=dirname(@__DIR__)))
+function dev_subpkg(subpkg)
+    subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
+    Pkg.develop(PackageSpec(path=subpkg_path))
+end
+
+function activate_subpkg_env(subpkg)
+    subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
+    Pkg.activate(subpkg_path)
+    Pkg.develop(PackageSpec(path=subpkg_path))
     Pkg.instantiate()
 end
 
 @time begin
 if GROUP == "All" || GROUP == "Core"
+    dev_subpkg("GalacticOptimJL")
+    dev_subpkg("GalacticFlux")
     @safetestset "AD Tests" begin include("ADtests.jl") end
     @safetestset "Mini batching" begin include("minibatch.jl") end
     @safetestset "DiffEqFlux" begin include("diffeqfluxtests.jl") end
 elseif GROUP == "GPU"
+    dev_subpkg("GalacticOptimJL")
+    dev_subpkg("GalacticFlux")
     activate_downstream_env()
     @safetestset "DiffEqFlux GPU" begin include("downstream/gpu_neural_ode.jl") end
 else
-    subpkg = joinpath(dirname(@__DIR__), "lib", GROUP)
-    Pkg.develop(PackageSpec(path=subpkg))
+    dev_subpkg(subpkg)
     include(joinpath(subpkg,"test","runtests.jl"))
 end
 end
