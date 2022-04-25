c2b50424adcfa1f37c09b9d0888280ae6ca2aa03
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2022-04-25 11:43:34 +0530

    Add GalacticMultistartOptimization

---
+++ GalacticMultistartOptimization/test/runtests.jl
@@ -0,0 +1,13 @@
+using GalacticMultistartOptimization, GalacticOptim, MultistartOptimization, NLopt, ForwardDiff
+using Test
+
+@testset "GalacticMultistartOptimization.jl" begin
+    rosenbrock(x, p) = (p[1] - x[1])^2 + p[2] * (x[2] - x[1]^2)^2
+    x0 = zeros(2)
+    _p = [1.0, 100.0]
+    l1 = rosenbrock(x0, _p)
+    f = OptimizationFunction(rosenbrock, GalacticOptim.AutoForwardDiff())
+    prob = GalacticOptim.OptimizationProblem(f, x0, _p, lb=[-1.0, -1.0], ub=[1.5, 1.5])
+    sol = solve(prob, MultistartOptimization.TikTak(100), NLopt.LD_LBFGS())
+    @test 10 * sol.minimum < l1
+end
