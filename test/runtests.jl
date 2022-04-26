fef96e9a52316505f97adc86fb623b7f28dedbdf
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2022-04-27 02:53:47 +0530

    Redefine `subpkg_path`

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,19 +18,28 @@ end
 if GROUP == "All" || GROUP == "Core"
     dev_subpkg("GalacticOptimJL")
     dev_subpkg("GalacticFlux")
-    @safetestset "AD Tests" begin include("ADtests.jl") end
-    @safetestset "Mini batching" begin include("minibatch.jl") end
-    @safetestset "DiffEqFlux" begin include("diffeqfluxtests.jl") end
+    @safetestset "AD Tests" begin
+        include("ADtests.jl")
+    end
+    @safetestset "Mini batching" begin
+        include("minibatch.jl")
+    end
+    @safetestset "DiffEqFlux" begin
+        include("diffeqfluxtests.jl")
+    end
 elseif GROUP == "GPU"
     dev_subpkg("GalacticOptimJL")
     dev_subpkg("GalacticFlux")
     activate_downstream_env()
-    @safetestset "DiffEqFlux GPU" begin include("downstream/gpu_neural_ode.jl") end
+    @safetestset "DiffEqFlux GPU" begin
+        include("downstream/gpu_neural_ode.jl")
+    end
 else
     dev_subpkg(GROUP)
     if GROUP == "MultistartOptimization"
         dev_subpkg("GalacticNLopt")
     end
-    Pkg.test(PackageSpec(name = GROUP, path=subpkg_path))
+    subpkg_path = joinpath(dirname(@__DIR__), "lib", GROUP)
+    Pkg.test(PackageSpec(name=GROUP, path=subpkg_path))
 end
 end
