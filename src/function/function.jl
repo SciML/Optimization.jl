ca1598d864e64a34f3d2b72670b7963444ad3464
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2022-05-27 16:39:58 +0530

    Pass prototype fields from passed function in `NoAD` dispatch

--- src/function/function.jl
+++ src/function/function.jl
@@ -5,8 +5,7 @@ function instantiate_function(f, x, ::AbstractADType, p, num_cons = 0)
     cons   = f.cons   === nothing ? nothing : (x)->f.cons(x,p)
     cons_j = f.cons_j === nothing ? nothing : (res,x)->f.cons_j(res,x,p)
     cons_h = f.cons_h === nothing ? nothing : (res,x)->f.cons_h(res,x,p)
-
-    return OptimizationFunction{true}(f.f, SciMLBase.NoAD(); grad=grad, hess=hess, hv=hv, 
+    return OptimizationFunction{true}(f.f, SciMLBase.NoAD(); grad=grad, hess=hess, hv=hv,
         cons=cons, cons_j=cons_j, cons_h=cons_h,
-        hess_prototype=nothing, cons_jac_prototype=nothing, cons_hess_prototype=nothing)
+        hess_prototype=f.hess_prototype, cons_jac_prototype=f.cons_jac_prototype, cons_hess_prototype=f.cons_hess_prototype)
 end
