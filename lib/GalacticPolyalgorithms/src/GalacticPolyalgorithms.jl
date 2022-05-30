693b1c5af900f0cf2ec796472f5a3b408181cd63
Vaibhav Kumar Dixit <vaibhavyashdixit@gmail.com>
2022-05-30 13:48:35 +0530

    Apply suggestions from code review

--- lib/GalacticPolyalgorithms/src/GalacticPolyalgorithms.jl
+++ lib/GalacticPolyalgorithms/src/GalacticPolyalgorithms.jl
@@ -20,9 +20,9 @@ function SciMLBase.__solve(prob::OptimizationProblem,
     if isempty(args) && deterministic && prob.lb === nothing && prob.ub === nothing
         # If determinsitic then ADAM -> finish with BFGS
         if maxiters === nothing
-            res1 = GalacticOptim.solve(optprob, Optimisers.ADAM(0.01), args...; maxiters=300, kwargs...)
+            res1 = GalacticOptim.solve(prob, Optimisers.ADAM(0.01), args...; maxiters=300, kwargs...)
         else
-            res1 = GalacticOptim.solve(optprob, Optimisers.ADAM(0.01), args...; maxiters, kwargs...)
+            res1 = GalacticOptim.solve(prob, Optimisers.ADAM(0.01), args...; maxiters, kwargs...)
         end
 
         optprob2 = GalacticOptim.OptimizationProblem(
