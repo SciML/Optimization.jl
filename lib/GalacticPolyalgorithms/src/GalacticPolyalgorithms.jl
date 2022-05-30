0457b2fde054924eec84ba158649db1af9459dcb
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-05-30 02:55:09 -0400

    fix lb ub names

--- lib/GalacticPolyalgorithms/src/GalacticPolyalgorithms.jl
+++ lib/GalacticPolyalgorithms/src/GalacticPolyalgorithms.jl
@@ -17,7 +17,7 @@ function SciMLBase.__solve(prob::OptimizationProblem,
         error("Automatic optimizer determination requires deterministic loss functions (and no data) or maxiters must be specified.")
     end
 
-    if isempty(args) && deterministic && lower_bounds === nothing && upper_bounds === nothing
+    if isempty(args) && deterministic && prob.lb === nothing && prob.ub === nothing
         # If determinsitic then ADAM -> finish with BFGS
         if maxiters === nothing
             res1 = GalacticOptim.solve(optprob, Optimisers.ADAM(0.01), args...; maxiters=300, kwargs...)
@@ -26,7 +26,7 @@ function SciMLBase.__solve(prob::OptimizationProblem,
         end
 
         optprob2 = GalacticOptim.OptimizationProblem(
-            optfunc, res1.u; lb=lower_bounds, ub=upper_bounds, kwargs...)
+            optfunc, res1.u; lb=prob.lb, ub=prob.ub, kwargs...)
         res1 = GalacticOptim.solve(
             optprob2, BFGS(initial_stepnorm=0.01), args...; maxiters, kwargs...)
     elseif isempty(args) && deterministic
