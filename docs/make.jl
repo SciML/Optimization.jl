d1991051e0817388c0479cccc7b4d5ead7fb332a
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-02 07:06:19 -0400

    separate out pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,8 @@
 using Documenter, Optimization
 using FiniteDiff, ForwardDiff, ModelingToolkit, ReverseDiff, Tracker, Zygote
 
+include("pages.jl")
+
 makedocs(
     sitename="Optimization.jl",
     authors="Chris Rackauckas, Vaibhav Kumar Dixit et al.",
@@ -13,38 +15,7 @@ makedocs(
                            assets=["assets/favicon.ico"],
                            canonical="https://Optimization.sciml.ai/stable/"),
 
-    pages=[
-        "Optimization.jl: A Unified Optimization Package" => "index.md",
-
-        "Tutorials" => [
-            "Basic usage" => "tutorials/intro.md",
-            "Rosenbrock function" => "tutorials/rosenbrock.md",
-            "Minibatch" => "tutorials/minibatch.md",
-            "Symbolic Modeling" => "tutorials/symbolic.md"
-        ],
-
-        "API" => [
-            "OptimizationProblem" => "API/optimization_problem.md",
-            "OptimizationFunction" => "API/optimization_function.md",
-            "solve" => "API/solve.md",
-            "ModelingToolkit Integration" => "API/modelingtoolkit.md"
-        ],
-        "Optimizer Packages" => [
-            "BlackBoxOptim.jl" => "optimization_packages/blackboxoptim.md",
-            "CMAEvolutionStrategy.jl" => "optimization_packages/cmaevolutionstrategy.md",
-            "Evolutionary.jl" => "optimization_packages/evolutionary.md",
-            "Flux.jl" => "optimization_packages/flux.md",
-            "GCMAES.jl" => "optimization_packages/gcmaes.md",
-            "MathOptInterface.jl" => "optimization_packages/mathoptinterface.md",
-            "MultistartOptimization.jl" => "optimization_packages/multistartoptimization.md",
-            "Metaheuristics.jl" => "optimization_packages/metaheuristics.md",
-            "NOMAD.jl" => "optimization_packages/nomad.md",
-            "NLopt.jl" => "optimization_packages/nlopt.md",
-            "Nonconvex.jl" => "optimization_packages/nonconvex.md",
-            "Optim.jl" => "optimization_packages/optim.md",
-            "QuadDIRECT.jl" => "optimization_packages/quaddirect.md"
-        ],
-    ]
+    pages=pages
 )
 
 deploydocs(
