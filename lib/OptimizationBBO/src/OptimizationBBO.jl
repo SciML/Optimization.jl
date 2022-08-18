ded00d7e7ff929ff364e6a3d2414eb212d03cbee
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2022-08-18 22:46:40 +0300

    Format log message

--- lib/OptimizationBBO/src/OptimizationBBO.jl
+++ lib/OptimizationBBO/src/OptimizationBBO.jl
@@ -13,7 +13,7 @@ function decompose_trace(opt::BlackBoxOptim.OptRunController, progress)
     if progress
         maxiters = opt.max_steps
         max_time = opt.max_time
-        msg = string(best_fitness(opt))
+        msg = sprint(show, best_fitness(opt), context=:compact => true)
         if iszero(max_time)
             # we stop at either convergence or max_steps
             n_steps = BlackBoxOptim.num_steps(opt)
