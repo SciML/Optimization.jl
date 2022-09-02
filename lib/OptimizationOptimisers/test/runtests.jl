86a8d1463849bd03c71ceb4c2af544478e1352e9
Kirill Zubov <kirillzubov3@gmail.com>
2022-09-02 04:19:07 -0400

    format

--- lib/OptimizationOptimisers/test/runtests.jl
+++ lib/OptimizationOptimisers/test/runtests.jl
@@ -18,13 +18,15 @@ using Test
     sol = solve(prob, Optimisers.ADAM(), maxiters = 1000, progress = false)
     @test 10 * sol.minimum < l1
 
-    x0 = 2*ones(ComplexF64, 2)
+    x0 = 2 * ones(ComplexF64, 2)
     _p = ones(2)
     sumfunc(x0, _p) = sum(abs2, (x0 - _p))
     l1 = sumfunc(x0, _p)
 
     optprob = OptimizationFunction(sumfunc, Optimization.AutoZygote())
-    prob = OptimizationProblem(optprob, x0,_p)
+
+    prob = OptimizationProblem(optprob, x0, _p)
+
     sol = solve(prob, Optimisers.ADAM(), maxiters = 1000)
     @test 10 * sol.minimum < l1
 end
