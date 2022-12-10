a55dc346d1624a574b8d0ce0f684973ecf76db60
Valentin Kaisermayer <valentin.kaisermayer@best-research.eu>
2022-12-10 10:41:38 +0100

    convert MOI status to SciMLBase retcode

--- lib/OptimizationMOI/src/OptimizationMOI.jl
+++ lib/OptimizationMOI/src/OptimizationMOI.jl
@@ -280,6 +280,50 @@ function __map_optimizer_args(prob::OptimizationProblem,
     return optimizer
 end
 
+function __moi_status_to_ReturnCode(status::MOI.TerminationStatusCode)
+    if status in [
+        MOI.OPTIMAL,
+        MOI.LOCALLY_SOLVED,
+        MOI.ALMOST_OPTIMAL,
+        MOI.ALMOST_LOCALLY_SOLVED,
+    ]
+        return ReturnCode.Success
+    elseif status in [
+        MOI.INFEASIBLE,
+        MOI.DUAL_INFEASIBLE,
+        MOI.LOCALLY_INFEASIBLE,
+        MOI.INFEASIBLE_OR_UNBOUNDED,
+        MOI.ALMOST_INFEASIBLE,
+        MOI.ALMOST_DUAL_INFEASIBLE,
+    ]
+        return ReturnCode.Infeasible
+    elseif status in [
+        MOI.ITERATION_LIMIT,
+        MOI.NODE_LIMIT,
+        MOI.SLOW_PROGRESS,
+    ]
+        return ReturnCode.MaxIters
+    elseif status == MOI.TIME_LIMIT
+        return ReturnCode.MaxTime
+    elseif status in [
+        MOI.OPTIMIZE_NOT_CALLED,
+        MOI.NUMERICAL_ERROR,
+        MOI.INVALID_MODEL,
+        MOI.INVALID_OPTION,
+        MOI.INTERRUPTED,
+        MOI.OTHER_ERROR,
+        MOI.SOLUTION_LIMIT,
+        MOI.MEMORY_LIMIT,
+        MOI.OBJECTIVE_LIMIT,
+        MOI.NORM_LIMIT,
+        MOI.OTHER_LIMIT,
+    ]
+        return ReturnCode.Failure
+    else
+        return ReturnCode.Default
+    end
+end
+
 function SciMLBase.__solve(prob::OptimizationProblem,
                            opt::Union{MOI.AbstractOptimizer, MOI.OptimizerWithAttributes};
                            maxiters::Union{Number, Nothing} = nothing,
@@ -337,11 +381,11 @@ function SciMLBase.__solve(prob::OptimizationProblem,
     if MOI.get(opt_setup, MOI.ResultCount()) >= 1
         minimizer = MOI.get(opt_setup, MOI.VariablePrimal(), θ)
         minimum = MOI.get(opt_setup, MOI.ObjectiveValue())
-        opt_ret = Symbol(string(MOI.get(opt_setup, MOI.TerminationStatus())))
+        opt_ret = __moi_status_to_ReturnCode(MOI.get(opt_setup, MOI.TerminationStatus()))
     else
         minimizer = fill(NaN, num_variables)
         minimum = NaN
-        opt_ret = :Default
+        opt_ret = ReturnCode.Default
     end
     return SciMLBase.build_solution(SciMLBase.DefaultOptimizationCache(prob.f, prob.p),
                                     opt,
