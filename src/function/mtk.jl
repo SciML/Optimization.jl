3f80add64aca22727a3b5984ba9000b1cf0d7097
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2023-01-21 13:52:01 +0530

    symbolify

--- src/function/mtk.jl
+++ src/function/mtk.jl
@@ -33,11 +33,11 @@ function instantiate_function(f, x, adtype::AutoModelingToolkit, p, num_cons = 0
     cons_j = (J, θ) -> f.cons_j(J, θ, p)
 
     cons_h = (res, θ) -> f.cons_h(res, θ, p)
-
     return OptimizationFunction{true}(f.f, adtype; grad = grad, hess = hess, hv = hv,
                                       cons = cons, cons_j = cons_j, cons_h = cons_h,
                                       hess_prototype = f.hess_prototype,
                                       cons_jac_prototype = f.cons_jac_prototype,
                                       cons_hess_prototype = f.cons_hess_prototype,
-                                      expr = f.expr, cons_expr = f.cons_expr)
+                                      expr = symbolify(f.expr),
+                                      cons_expr = symbolify.(f.cons_expr))
 end
