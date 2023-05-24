0c56a6d78c06200148c5a098b756e1acdaf94d6e
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2023-05-24 12:54:42 +0530

    Improve error message when AD pkg not loaded

--- src/function.jl
+++ src/function.jl
@@ -103,6 +103,11 @@ end
 
 function instantiate_function(f, x, adtype::ADTypes.AbstractADType,
                               p, num_cons = 0)
-    adpkg = string(adtype)[5:(end - 2)]
+    adtypestr = string(adtype)
+    open_nrmlbrkt_ind = findfirst('(', adtypestr)
+    open_squigllybrkt_ind = findfirst('{', adtypestr)
+    open_brkt_ind = isnothing(open_squigllybrkt_ind) ? open_nrmlbrkt_ind :
+                    min(open_nrmlbrkt_ind, open_squigllybrkt_ind)
+    adpkg = adtypestr[5:(open_brkt_ind - 1)]
     throw(ArgumentError("The passed automatic differentiation backend choice is not available. Please load the corresponding AD package $adpkg."))
 end
