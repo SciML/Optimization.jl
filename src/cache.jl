170844066f504c05c7e8db4bd40c1b6141b5eedc
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2023-06-07 00:13:35 +0530

    Add kwargs to OptimizationCache constructor

--- src/cache.jl
+++ src/cache.jl
@@ -51,14 +51,21 @@ struct OptimizationCache{F, RC, LB, UB, LC, UC, S, O, D, P, C} <:
     solver_args::NamedTuple
 end
 
-function OptimizationCache(prob::OptimizationProblem, opt, data; progress, callback,
-                                  kwargs...)
+function OptimizationCache(prob::SciMLBase.OptimizationProblem, opt, data;
+                           callback = (args...) -> (false),
+                           maxiters::Union{Number, Nothing} = nothing,
+                           maxtime::Union{Number, Nothing} = nothing,
+                           abstol::Union{Number, Nothing} = nothing,
+                           reltol::Union{Number, Nothing} = nothing,
+                           progress = false,
+                           kwargs...)
     reinit_cache = Optimization.ReInitCache(prob.u0, prob.p)
     num_cons = prob.ucons === nothing ? 0 : length(prob.ucons)
     f = Optimization.instantiate_function(prob.f, reinit_cache, prob.f.adtype, num_cons)
     return OptimizationCache(f, reinit_cache, prob.lb, prob.ub, prob.lcons,
                                 prob.ucons, prob.sense,
-                                opt, data, progress, callback, NamedTuple(kwargs))
+                                opt, data, progress, callback,
+                                merge((; maxiters, maxtime, abstol, reltol), NamedTuple(kwargs)))
 end
 
 function SciMLBase.__init(prob::SciMLBase.OptimizationProblem, opt,
