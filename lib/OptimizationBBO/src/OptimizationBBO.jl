87f72a92a7522609cdff844d523c40a6bf769b08
Zentrik <Zentrik@users.noreply.github.com>
2023-10-01 01:09:47 +0100

    Use `===` instead of `==`
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

--- lib/OptimizationBBO/src/OptimizationBBO.jl
+++ lib/OptimizationBBO/src/OptimizationBBO.jl
@@ -112,7 +112,7 @@ function SciMLBase.__solve(cache::Optimization.OptimizationCache{
     cur, state = iterate(cache.data)
 
     function _cb(trace)
-        if cache.callback == Optimization.DEFAULT_CALLBACK
+        if cache.callback === Optimization.DEFAULT_CALLBACK
             cb_call = false
         else
             cb_call = cache.callback(decompose_trace(trace, cache.progress), x...)
@@ -125,7 +125,7 @@ function SciMLBase.__solve(cache::Optimization.OptimizationCache{
             BlackBoxOptim.shutdown_optimizer!(trace) #doesn't work
         end
 
-        if cache.data != Optimization.DEFAULT_DATA
+        if cache.data !== Optimization.DEFAULT_DATA
             cur, state = iterate(cache.data, state)
         end
         cb_call
@@ -135,11 +135,11 @@ function SciMLBase.__solve(cache::Optimization.OptimizationCache{
     maxtime = Optimization._check_and_convert_maxtime(cache.solver_args.maxtime)
 
     _loss = function (θ)
-        if cache.callback == Optimization.DEFAULT_CALLBACK && cache.data == Optimization.DEFAULT_DATA
+        if cache.callback === Optimization.DEFAULT_CALLBACK && cache.data === Optimization.DEFAULT_DATA
             return first(cache.f(θ, cache.p))
-        elseif cache.callback == Optimization.DEFAULT_CALLBACK
+        elseif cache.callback === Optimization.DEFAULT_CALLBACK
             return first(cache.f(θ, cache.p, cur...))
-        elseif cache.data != Optimization.DEFAULT_DATA
+        elseif cache.data !== Optimization.DEFAULT_DATA
             x = cache.f(θ, cache.p)
             return first(x)
         else
@@ -149,8 +149,8 @@ function SciMLBase.__solve(cache::Optimization.OptimizationCache{
     end
 
     opt_args = __map_optimizer_args(cache, cache.opt;
-        callback = cache.callback == Optimization.DEFAULT_CALLBACK &&
-                   cache.data == Optimization.DEFAULT_DATA ?
+        callback = cache.callback === Optimization.DEFAULT_CALLBACK &&
+                   cache.data === Optimization.DEFAULT_DATA ?
                    nothing : _cb,
         cache.solver_args...,
         maxiters = maxiters,
