299f4cd386867de77e3e2823ea89dae27c23f553
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2023-10-30 12:50:26 -0400

    Use SciMLBase ensemble solve

--- src/ensemble.jl
+++ src/ensemble.jl
@@ -3,18 +3,7 @@ function SciMLBase.EnsembleProblem(prob::OptimizationProblem, u0s::Vector{Vector
     return SciMLBase.EnsembleProblem(prob; prob_func, kwargs...)
 end
 
-function SciMLBase.init(prob::EnsembleProblem{T}, args...; kwargs...) where {T <: OptimizationProblem}
-    SciMLBase.__init(prob, args...; kwargs...)
-end
 
-function SciMLBase.__init(prob::EnsembleProblem{T}, args...; trajectories,  kwargs...) where {T <: OptimizationProblem}
-    return [SciMLBase.__init(prob.prob_func(prob.prob, i), args...; kwargs...) for i in 1:trajectories]
-end
-
-function SciMLBase.solve!(cache::Vector{<:OptimizationCache}; kwargs...)
-    return [SciMLBase.solve!(cache[i]; kwargs...) for i in eachindex(cache)]
-end
-
-function SciMLBase.__solve(caches::Vector{<:OptimizationCache}, args...; kwargs...)
-    return [SciMLBase.__solve(cache, args...; kwargs...) for cache in caches]
+function SciMLBase.solve(prob::EnsembleProblem{T}, args...; kwargs...) where {T <: OptimizationProblem}
+    return SciMLBase.__solve(prob, args...; kwargs...)
 end
--- test/ensemble.jl
+++ test/ensemble.jl
@@ -1,4 +1,4 @@
-using Optimization, OptimizationOptimJL, ForwardDiff
+using Optimization, OptimizationOptimJL, ForwardDiff, Test
 
 x0 = zeros(2)
 rosenbrock(x, p = nothing) = (1 - x[1])^2 + 100 * (x[2] - x[1]^2)^2
@@ -10,6 +10,9 @@ sol1 = Optimization.solve(prob, OptimizationOptimJL.BFGS(), maxiters = 5)
 
 
 ensembleprob = Optimization.EnsembleProblem(prob, [x0, x0 .+ rand(2), x0 .+ rand(2), x0 .+ rand(2)])
-sol = Optimization.solve(ensembleprob, OptimizationOptimJL.BFGS(), trajectories = 4, maxiters = 5)
 
-@test findmin(i -> sol[i].objective, 1:4) < sol1.objective
+sol = Optimization.solve(ensembleprob, OptimizationOptimJL.BFGS(), EnsembleThreads(), trajectories = 4, maxiters = 5)
+@test findmin(i -> sol[i].objective, 1:4)[1] < sol1.objective
+
+sol = Optimization.solve(ensembleprob, OptimizationOptimJL.BFGS(), EnsembleDistributed(), trajectories = 4, maxiters = 5)
+@test findmin(i -> sol[i].objective, 1:4)[1] < sol1.objective
