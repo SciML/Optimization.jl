136606b0e3d470b221885326af6e8e699662ee34
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2024-03-04 19:03:44 -0500

    Move code from existing PR

---
+++ lib/OptimizationManopt/src/OptimizationManopt.jl
@@ -0,0 +1,306 @@
+module OptimizationManopt
+
+using Optimization, Manopt, ManifoldsBase
+
+"""
+    abstract type AbstractManoptOptimizer end
+
+A Manopt solver without things specified by a call to `solve` (stopping criteria) and
+internal state.
+"""
+abstract type AbstractManoptOptimizer end
+
+function stopping_criterion_to_kwarg(stopping_criterion::Nothing)
+    return NamedTuple()
+end
+function stopping_criterion_to_kwarg(stopping_criterion::StoppingCriterion)
+    return (; stopping_criterion = stopping_criterion)
+end
+
+## gradient descent
+
+struct GradientDescentOptimizer{
+                                Teval <: AbstractEvaluationType,
+                                TM <: AbstractManifold,
+                                TLS <: Linesearch
+                                } <: AbstractManoptOptimizer
+    M::TM
+    stepsize::TLS
+end
+
+function GradientDescentOptimizer(M::AbstractManifold;
+                                  eval::AbstractEvaluationType = MutatingEvaluation(),
+                                  stepsize::Stepsize = ArmijoLinesearch(M))
+    GradientDescentOptimizer{typeof(eval), typeof(M), typeof(stepsize)}(M, stepsize)
+end
+
+function call_manopt_optimizer(opt::GradientDescentOptimizer{Teval},
+                               loss,
+                               gradF,
+                               x0,
+                               stopping_criterion::Union{Nothing, Manopt.StoppingCriterion}) where {
+                                                                                                    Teval <:
+                                                                                                    AbstractEvaluationType
+                                                                                                    }
+    sckwarg = stopping_criterion_to_kwarg(stopping_criterion)
+    opts = gradient_descent(opt.M,
+                            loss,
+                            gradF,
+                            x0;
+                            return_options = true,
+                            evaluation = Teval(),
+                            stepsize = opt.stepsize,
+                            sckwarg...)
+    # we unwrap DebugOptions here
+    minimizer = Manopt.get_solver_result(opts)
+    return (; minimizer = minimizer, minimum = loss(opt.M, minimizer), options = opts),
+           :who_knows
+end
+
+## Nelder-Mead
+
+struct NelderMeadOptimizer{
+                           TM <: AbstractManifold,
+                           Tpop <: AbstractVector
+                           } <: AbstractManoptOptimizer
+    M::TM
+    initial_population::Tpop
+end
+
+function NelderMeadOptimizer(M::AbstractManifold)
+    initial_population = [rand(M) for _ in 1:(manifold_dimension(M) + 1)]
+    return NelderMeadOptimizer{typeof(M), typeof(initial_population)}(M, initial_population)
+end
+
+function call_manopt_optimizer(opt::NelderMeadOptimizer,
+                               loss,
+                               gradF,
+                               x0,
+                               stopping_criterion::Union{Nothing, Manopt.StoppingCriterion})
+    sckwarg = stopping_criterion_to_kwarg(stopping_criterion)
+
+    opts = NelderMead(opt.M,
+                      loss,
+                      opt.initial_population;
+                      return_options = true,
+                      sckwarg...)
+    minimizer = Manopt.get_solver_result(opts)
+    return (; minimizer = minimizer, minimum = loss(opt.M, minimizer), options = opts),
+           :who_knows
+end
+
+## conjugate gradient descent
+
+struct ConjugateGradientDescentOptimizer{Teval <: AbstractEvaluationType,
+                                         TM <: AbstractManifold, TLS <: Stepsize} <:
+       AbstractManoptOptimizer
+    M::TM
+    stepsize::TLS
+end
+
+function ConjugateGradientDescentOptimizer(M::AbstractManifold;
+                                           eval::AbstractEvaluationType = MutatingEvaluation(),
+                                           stepsize::Stepsize = ArmijoLinesearch(M))
+    ConjugateGradientDescentOptimizer{typeof(eval), typeof(M), typeof(stepsize)}(M,
+                                                                                 stepsize)
+end
+
+function call_manopt_optimizer(opt::ConjugateGradientDescentOptimizer{Teval},
+                               loss,
+                               gradF,
+                               x0,
+                               stopping_criterion::Union{Nothing, Manopt.StoppingCriterion}) where {
+                                                                                                    Teval <:
+                                                                                                    AbstractEvaluationType
+                                                                                                    }
+    sckwarg = stopping_criterion_to_kwarg(stopping_criterion)
+    opts = conjugate_gradient_descent(opt.M,
+                                      loss,
+                                      gradF,
+                                      x0;
+                                      return_options = true,
+                                      evaluation = Teval(),
+                                      stepsize = opt.stepsize,
+                                      sckwarg...)
+    # we unwrap DebugOptions here
+    minimizer = Manopt.get_solver_result(opts)
+    return (; minimizer = minimizer, minimum = loss(opt.M, minimizer), options = opts),
+           :who_knows
+end
+
+## particle swarm
+
+struct ParticleSwarmOptimizer{Teval <: AbstractEvaluationType,
+                              TM <: AbstractManifold, Tretr <: AbstractRetractionMethod,
+                              Tinvretr <: AbstractInverseRetractionMethod,
+                              Tvt <: AbstractVectorTransportMethod} <:
+       AbstractManoptOptimizer
+    M::TM
+    retraction_method::Tretr
+    inverse_retraction_method::Tinvretr
+    vector_transport_method::Tvt
+    population_size::Int
+end
+
+function ParticleSwarmOptimizer(M::AbstractManifold;
+                                eval::AbstractEvaluationType = MutatingEvaluation(),
+                                population_size::Int = 100,
+                                retraction_method::AbstractRetractionMethod = default_retraction_method(M),
+                                inverse_retraction_method::AbstractInverseRetractionMethod = default_inverse_retraction_method(M),
+                                vector_transport_method::AbstractVectorTransportMethod = default_vector_transport_method(M))
+    ParticleSwarmOptimizer{typeof(eval), typeof(M), typeof(retraction_method),
+                           typeof(inverse_retraction_method),
+                           typeof(vector_transport_method)}(M,
+                                                            retraction_method,
+                                                            inverse_retraction_method,
+                                                            vector_transport_method,
+                                                            population_size)
+end
+
+function call_manopt_optimizer(opt::ParticleSwarmOptimizer{Teval},
+                               loss,
+                               gradF,
+                               x0,
+                               stopping_criterion::Union{Nothing, Manopt.StoppingCriterion}) where {
+                                                                                                    Teval <:
+                                                                                                    AbstractEvaluationType
+                                                                                                    }
+    sckwarg = stopping_criterion_to_kwarg(stopping_criterion)
+    initial_population = vcat([x0], [rand(opt.M) for _ in 1:(opt.population_size - 1)])
+    opts = particle_swarm(opt.M,
+                          loss;
+                          x0 = initial_population,
+                          n = opt.population_size,
+                          return_options = true,
+                          retraction_method = opt.retraction_method,
+                          inverse_retraction_method = opt.inverse_retraction_method,
+                          vector_transport_method = opt.vector_transport_method,
+                          sckwarg...)
+    # we unwrap DebugOptions here
+    minimizer = Manopt.get_solver_result(opts)
+    return (; minimizer = minimizer, minimum = loss(opt.M, minimizer), options = opts),
+           :who_knows
+end
+
+## quasi Newton
+
+struct QuasiNewtonOptimizer{Teval <: AbstractEvaluationType,
+                            TM <: AbstractManifold, Tretr <: AbstractRetractionMethod,
+                            Tvt <: AbstractVectorTransportMethod, TLS <: Stepsize} <:
+       AbstractManoptOptimizer
+    M::TM
+    retraction_method::Tretr
+    vector_transport_method::Tvt
+    stepsize::TLS
+end
+
+function QuasiNewtonOptimizer(M::AbstractManifold;
+                              eval::AbstractEvaluationType = MutatingEvaluation(),
+                              retraction_method::AbstractRetractionMethod = default_retraction_method(M),
+                              vector_transport_method::AbstractVectorTransportMethod = default_vector_transport_method(M),
+                              stepsize = WolfePowellLinesearch(M;
+                                                               retraction_method = retraction_method,
+                                                               vector_transport_method = vector_transport_method,
+                                                               linesearch_stopsize = 1e-12))
+    QuasiNewtonOptimizer{typeof(eval), typeof(M), typeof(retraction_method),
+                         typeof(vector_transport_method), typeof(stepsize)}(M,
+                                                                            retraction_method,
+                                                                            vector_transport_method,
+                                                                            stepsize)
+end
+
+function call_manopt_optimizer(opt::QuasiNewtonOptimizer{Teval},
+                               loss,
+                               gradF,
+                               x0,
+                               stopping_criterion::Union{Nothing, Manopt.StoppingCriterion}) where {
+                                                                                                    Teval <:
+                                                                                                    AbstractEvaluationType
+                                                                                                    }
+    sckwarg = stopping_criterion_to_kwarg(stopping_criterion)
+    opts = quasi_Newton(opt.M,
+                                      loss,
+                                      gradF,
+                                      x0;
+                                      return_options = true,
+                                      evaluation = Teval(),
+                                      retraction_method = opt.retraction_method,
+                                      vector_transport_method = opt.vector_transport_method,
+                                      stepsize = opt.stepsize,
+                                      sckwarg...)
+    # we unwrap DebugOptions here
+    minimizer = Manopt.get_solver_result(opts)
+    return (; minimizer = minimizer, minimum = loss(opt.M, minimizer), options = opts),
+           :who_knows
+end
+
+## Optimization.jl stuff
+
+function build_loss(f::OptimizationFunction, prob, cur)
+    function (::AbstractManifold, θ)
+        x = f.f(θ, prob.p, cur...)
+        __x = first(x)
+        return prob.sense === Optimization.MaxSense ? -__x : __x
+    end
+end
+
+function build_gradF(f::OptimizationFunction{true}, prob, cur)
+    function (::AbstractManifold, G, θ)
+        X = f.grad(G, θ, cur...)
+        if prob.sense === Optimization.MaxSense
+            return -X # TODO: check
+        else
+            return X
+        end
+    end
+end
+
+# TODO:
+# 1) convert tolerances and other stopping criteria
+# 2) return convergence information
+# 3) add callbacks to Manopt.jl
+
+function SciMLBase.__solve(prob::OptimizationProblem,
+                           opt::AbstractManoptOptimizer,
+                           data = Optimization.DEFAULT_DATA;
+                           callback = (args...) -> (false),
+                           maxiters::Union{Number, Nothing} = nothing,
+                           maxtime::Union{Number, Nothing} = nothing,
+                           abstol::Union{Number, Nothing} = nothing,
+                           reltol::Union{Number, Nothing} = nothing,
+                           progress = false,
+                           kwargs...)
+    local x, cur, state
+
+    if data !== Optimization.DEFAULT_DATA
+        maxiters = length(data)
+    end
+
+    cur, state = iterate(data)
+
+    stopping_criterion = nothing
+    if maxiters !== nothing
+        stopping_criterion = StopAfterIteration(maxiters)
+    end
+
+    maxiters = Optimization._check_and_convert_maxiters(maxiters)
+    maxtime = Optimization._check_and_convert_maxtime(maxtime)
+
+    f = Optimization.instantiate_function(prob.f, prob.u0, prob.f.adtype, prob.p)
+
+    _loss = build_loss(f, prob, cur)
+
+    gradF = build_gradF(f, prob, cur)
+
+    opt_res, opt_ret = call_manopt_optimizer(opt, _loss, gradF, prob.u0, stopping_criterion)
+
+    return SciMLBase.build_solution(SciMLBase.DefaultOptimizationCache(prob.f, prob.p),
+                                    opt,
+                                    opt_res.minimizer,
+                                    prob.sense === Optimization.MaxSense ?
+                                    -opt_res.minimum : opt_res.minimum;
+                                    original = opt_res.options,
+                                    retcode = opt_ret)
+end
+
+end # module OptimizationManopt
---
+++ lib/OptimizationManopt/test/runtests.jl
@@ -0,0 +1,101 @@
+using OptimizationManopt
+using Optimization
+using Manifolds
+using ForwardDiff
+using Manopt
+using Test
+using SciMLBase
+
+rosenbrock(x, p) = (p[1] - x[1])^2 + p[2] * (x[2] - x[1]^2)^2
+
+function rosenbrock_grad!(storage, x, p)
+    storage[1] = -2.0 * (p[1] - x[1]) - 4.0 * p[2] * (x[2] - x[1]^2) * x[1]
+    storage[2] = 2.0 * p[2] * (x[2] - x[1]^2)
+end
+
+R2 = Euclidean(2)
+
+@testset "Gradient descent" begin
+    x0 = zeros(2)
+    p = [1.0, 100.0]
+
+    stepsize = Manopt.ArmijoLinesearch(R2)
+    opt = OptimizationManopt.GradientDescentOptimizer(R2,
+                                                      stepsize = stepsize)
+
+    optprob_forwarddiff = OptimizationFunction(rosenbrock, Optimization.AutoForwardDiff())
+    prob_forwarddiff = OptimizationProblem(optprob_forwarddiff, x0, p)
+    sol = Optimization.solve(prob_forwarddiff, opt)
+    @test sol.minimum < 0.2
+
+    optprob_grad = OptimizationFunction(rosenbrock; grad = rosenbrock_grad!)
+    prob_grad = OptimizationProblem(optprob_grad, x0, p)
+    sol = Optimization.solve(prob_grad, opt)
+    @test sol.minimum < 0.2
+end
+
+@testset "Nelder-Mead" begin
+    x0 = zeros(2)
+    p = [1.0, 100.0]
+
+    opt = OptimizationManopt.NelderMeadOptimizer(R2, [[1.0, 0.0], [0.0, 1.0], [0.0, 0.0]])
+
+    optprob = OptimizationFunction(rosenbrock)
+    prob = OptimizationProblem(optprob, x0, p)
+
+    sol = Optimization.solve(prob, opt)
+    @test sol.minimum < 0.7
+end
+
+@testset "Conjugate gradient descent" begin
+    x0 = zeros(2)
+    p = [1.0, 100.0]
+
+    stepsize = Manopt.ArmijoLinesearch(R2)
+    opt = OptimizationManopt.ConjugateGradientDescentOptimizer(R2,
+                                                               stepsize = stepsize)
+
+    optprob = OptimizationFunction(rosenbrock, Optimization.AutoForwardDiff())
+    prob = OptimizationProblem(optprob, x0, p)
+
+    sol = Optimization.solve(prob, opt)
+    @test sol.minimum < 0.2
+end
+
+@testset "Quasi Newton" begin
+    x0 = zeros(2)
+    p = [1.0, 100.0]
+
+    opt = OptimizationManopt.QuasiNewtonOptimizer(R2)
+
+    optprob = OptimizationFunction(rosenbrock, Optimization.AutoForwardDiff())
+    prob = OptimizationProblem(optprob, x0, p)
+
+    sol = Optimization.solve(prob, opt)
+    @test sol.minimum < 1e-16
+end
+
+@testset "Particle swarm" begin
+    x0 = zeros(2)
+    p = [1.0, 100.0]
+
+    opt = OptimizationManopt.ParticleSwarmOptimizer(R2)
+
+    optprob = OptimizationFunction(rosenbrock)
+    prob = OptimizationProblem(optprob, x0, p)
+
+    sol = Optimization.solve(prob, opt)
+    @test sol.minimum < 0.1
+end
+
+@testset "Custom constraints" begin
+    cons(res, x, p) = (res .= [x[1]^2 + x[2]^2, x[1] * x[2]])
+
+    x0 = zeros(2)
+    p = [1.0, 100.0]
+    opt = OptimizationManopt.GradientDescentOptimizer(R2)
+
+    optprob_cons = OptimizationFunction(rosenbrock; grad = rosenbrock_grad!, cons = cons)
+    prob_cons = OptimizationProblem(optprob_cons, x0, p)
+    @test_throws SciMLBase.IncompatibleOptimizerError Optimization.solve(prob_cons, opt)
+end
