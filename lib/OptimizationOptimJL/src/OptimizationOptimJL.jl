09d9fc70e841bb3f10bf8f6ebc1f7bb1b363586d
Paras Puneet Singh <136245940+ParasPuneetSingh@users.noreply.github.com>
2024-03-05 17:21:21 +0530

    Update OptimizationOptimJL.jl
    Adding trait for requiring derivatives and throwing error to use OptimizationFunction

--- lib/OptimizationOptimJL/src/OptimizationOptimJL.jl
+++ lib/OptimizationOptimJL/src/OptimizationOptimJL.jl
@@ -14,13 +14,7 @@ SciMLBase.requiresbounds(opt::Optim.SAMIN) = true
 SciMLBase.supports_opt_cache_interface(opt::Optim.AbstractOptimizer) = true
 SciMLBase.supports_opt_cache_interface(opt::Union{Optim.Fminbox, Optim.SAMIN}) = true
 SciMLBase.supports_opt_cache_interface(opt::Optim.ConstrainedOptimizer) = true
-SciMLBase.requiresgradient(opt::Optim.AbstractOptimizer) = !(opt isa Optim.ZerothOrderOptimizer)
-SciMLBase.requiresgradient(::IPNewton) = true
-SciMLBase.requireshessian(::IPNewton) = true
-SciMLBase.requiresconsjac(::IPNewton) = true
-SciMLBase.requireshessian(opt::Optim.NewtonTrustRegion) = true
-SciMLBase.requireshessian(opt::Optim.Newton) = true
-SciMLBase.requiresgradient(opt::Optim.Fminbox) = true
+SciMLBase.requiresderivative(Optim.AbstractOptimizer) = !(opt isa Optim.ZerothOrderOptimizer)
 
 function __map_optimizer_args(cache::OptimizationCache,
         opt::Union{Optim.AbstractOptimizer, Optim.Fminbox,
@@ -135,6 +129,8 @@ function SciMLBase.__solve(cache::OptimizationCache{
     local x, cur, state
 
     cur, state = iterate(cache.data)
+    (requiresderivative(Optim.AbstractOptimizer))
+        error("Use OptimizationFunction to pass the derivatives or automatically generate them with one of the autodiff backends")
     !(cache.opt isa Optim.ZerothOrderOptimizer) && cache.f.grad === nothing &&
         error("Use OptimizationFunction to pass the derivatives or automatically generate them with one of the autodiff backends")
 
