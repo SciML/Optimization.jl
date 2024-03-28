7b19429b7eaf228d9d256c68de73d1335bd6e80e
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2024-03-27 21:51:04 -0400

    Try augmented-lagrangian with lbfgsb

--- src/lbfgsb.jl
+++ src/lbfgsb.jl
@@ -19,6 +19,7 @@ end
 SciMLBase.supports_opt_cache_interface(::LBFGS) = true
 SciMLBase.allowsbounds(::LBFGS) = true
 # SciMLBase.requiresgradient(::LBFGS) = true
+SciMLBase.allowsconstraints(::LBFGS) = true
 
 function SciMLBase.__init(prob::SciMLBase.OptimizationProblem,
         opt::LBFGS,
@@ -65,26 +66,109 @@ function SciMLBase.__solve(cache::OptimizationCache{
 
     local x
 
-    _loss = function (θ)
-        x = cache.f(θ, cache.p)
-        opt_state = Optimization.OptimizationState(u = θ, objective = x[1])
-        if cache.callback(opt_state, x...)
-            error("Optimization halted by callback.")
+    if !isnothing(cache.f.cons) 
+        eq_inds = [cache.lcons[i] == cache.ucons[i] for i in eachindex(cache.lcons)]
+        ineq_inds = (!).(eq_inds)
+
+        τ = 0.5
+        γ = 10.0
+        λmin = -1e20
+        λmax = 1e20
+        μmin = 0.0
+        μmax = 1e20
+        ϵ = 1e-8
+
+        λ = zeros(eltype(cache.u0), sum(eq_inds))
+        μ = zeros(eltype(cache.u0), sum(ineq_inds))
+
+        cons_tmp = zeros(eltype(cache.u0), length(cache.lcons))
+        cache.f.cons(cons_tmp, cache.u0)
+        ρ = max(1e-6, min(10, 2*(abs(cache.f(cache.u0, cache.p)))/ norm(cons_tmp) ))
+
+        _loss = function (θ)
+            x = cache.f(θ, cache.p)
+            cons_tmp .= 0.0 
+            cache.f.cons(cons_tmp, θ)
+            opt_state = Optimization.OptimizationState(u = θ, objective = x[1])
+            if cache.callback(opt_state, x...)
+                error("Optimization halted by callback.")
+            end
+            return x[1] + sum(@. λ * cons_tmp[eq_inds] + ρ/2 * (cons_tmp[eq_inds].^2)) + 0.5/ ρ * sum((max.(Ref(0.0), μ .+ (ρ .* cons_tmp[ineq_inds]))).^2)
         end
-        return x[1]
-    end
 
-    t0 = time()
-    if cache.lb !== nothing && cache.ub !== nothing
-        res = lbfgsb(_loss, cache.f.grad, cache.u0; m = cache.opt.m, maxiter = maxiters,
-            lb = cache.lb, ub = cache.ub)
+        prev_eqcons = zero(λ)
+        θ = cache.u0
+        β = max.(cons_tmp[ineq_inds], Ref(0.0))
+        prevβ = zero(β)
+        eqidxs =  [eq_inds[i] > 0 ? i : nothing for i in eachindex(ineq_inds)]
+        ineqidxs = [ineq_inds[i] > 0 ? i : nothing for i in eachindex(ineq_inds)]
+        eqidxs = eqidxs[eqidxs.!=nothing]
+        ineqidxs = ineqidxs[ineqidxs.!=nothing]
+        function aug_grad(G, θ)
+            cache.f.grad(G, θ)
+            J = zeros((length(cache.lcons), length(θ)))
+            cache.f.cons_j(J, θ)
+            __tmp = zero(cons_tmp)
+            cache.f.cons(__tmp, θ)
+            G .+= sum(λ .* J[i, :] + ρ * (__tmp[eq_inds].* J[i, :]) for i in eqidxs)
+            G .+= sum(ρ * (max.(Ref(0.0), μ .+ (ρ .* __tmp[ineq_inds])) .* J[i, :]) for i in ineqidxs)
+        end
+        for i in 1:maxiters
+            prev_eqcons .= cons_tmp[eq_inds]
+            prevβ .= copy(β)
+            if cache.lb !== nothing && cache.ub !== nothing
+                res = lbfgsb(_loss, aug_grad, θ; m = cache.opt.m, pgtol = sqrt(ϵ), maxiter = 100, lb = cache.lb, ub = cache.ub)
+            else
+                res = lbfgsb(_loss, aug_grad, θ; m = cache.opt.m, pgtol = sqrt(ϵ), maxiter = 100)
+            end
+            @show res[2]
+            @show res[1]
+            @show cons_tmp
+            @show λ
+            @show β
+            @show μ
+
+            θ = res[2]
+            cons_tmp .= 0.0
+            cache.f.cons(cons_tmp, θ)
+            λ = max.(min.(λmax , λ .+ ρ * cons_tmp[eq_inds]), λmin)
+            β = max.(cons_tmp[ineq_inds], -1 .* μ ./ ρ)
+            μ = min.(μmax, max.(μ .+ ρ * cons_tmp[ineq_inds], μmin))
+
+            if max(norm(cons_tmp[eq_inds], Inf), norm(β, Inf)) > τ * max(norm(prev_eqcons, Inf), norm(prevβ, Inf))
+                ρ = γ * ρ
+            end
+            if norm(cons_tmp[eq_inds], Inf) < ϵ && norm(β, Inf) < ϵ
+                break
+            end
+        end
+
+        stats = Optimization.OptimizationStats(; iterations = maxiters,
+            time = 0.0, fevals = maxiters, gevals = maxiters)
+        return SciMLBase.build_solution(cache, cache.opt, res[2], res[1], stats = stats)
     else
-        res = lbfgsb(_loss, cache.f.grad, cache.u0; m = cache.opt.m, maxiter = maxiters)
+        _loss = function (θ)
+            x = cache.f(θ, cache.p)
+
+            opt_state = Optimization.OptimizationState(u = θ, objective = x[1])
+            if cache.callback(opt_state, x...)
+                error("Optimization halted by callback.")
+            end
+            return x[1]
+        end
+
+        t0 = time()
+        if cache.lb !== nothing && cache.ub !== nothing
+            res = lbfgsb(_loss, cache.f.grad, cache.u0; m = cache.opt.m, maxiter = maxiters,
+                lb = cache.lb, ub = cache.ub)
+        else
+            res = lbfgsb(_loss, cache.f.grad, cache.u0; m = cache.opt.m, maxiter = maxiters)
+        end
+
+        t1 = time()
+        stats = Optimization.OptimizationStats(; iterations = maxiters,
+            time = t1 - t0, fevals = maxiters, gevals = maxiters)
+
+        return SciMLBase.build_solution(cache, cache.opt, res[2], res[1], stats = stats)
     end
-
-    t1 = time()
-    stats = Optimization.OptimizationStats(; iterations = maxiters,
-        time = t1 - t0, fevals = maxiters, gevals = maxiters)
-
-    return SciMLBase.build_solution(cache, cache.opt, res[2], res[1], stats = stats)
 end
--- test/lbfgsb.jl
+++ test/lbfgsb.jl
@@ -1,6 +1,7 @@
 using Optimization
 using ForwardDiff, Zygote, ReverseDiff, FiniteDiff, Tracker
 using ModelingToolkit, Enzyme, Random
+using Test
 
 x0 = zeros(2)
 rosenbrock(x, p = nothing) = (1 - x[1])^2 + 100 * (x[2] - x[1]^2)^2
@@ -17,3 +18,11 @@ prob = OptimizationProblem(optf, x0, lb = [0.0, 0.0], ub = [0.3, 0.3])
 res = solve(prob, Optimization.LBFGS(), maxiters = 100)
 
 @test res.u≈[0.3, 0.09] atol=1e-3
+
+function con2_c(res, x, p)
+    res .= [x[1]^2 + x[2]^2, 5 - (x[2] * sin(x[1]) - x[1])]
+end
+
+optf = OptimizationFunction(rosenbrock, AutoForwardDiff(), cons = con2_c)
+prob = OptimizationProblem(optf, x0, lcons = [2, -Inf], ucons = [2.0, 0.0])
+res = solve(prob, Optimization.LBFGS(), maxiters = 10)
