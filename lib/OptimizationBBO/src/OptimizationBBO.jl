9d7892b7ee667986bf41e47c4b23c6da2835c04c
Paras Puneet Singh <136245940+ParasPuneetSingh@users.noreply.github.com>
2024-05-29 13:18:04 +0530

    Proposed solution to bug #745
    The comment edit needs to be performed in SciMLBase and other solver packages.

--- lib/OptimizationBBO/src/OptimizationBBO.jl
+++ lib/OptimizationBBO/src/OptimizationBBO.jl
@@ -182,7 +182,11 @@ function SciMLBase.__solve(cache::Optimization.OptimizationCache{
     end
 
     t1 = time()
-
+"""
+the retcode below is generated and returned to SciMLBase.build_solution function needs to have a case where if not 
+recognised and the method converges we return success or special warning that the retcode was not recognised 
+and if the process was successful ignore the warning.
+"""
     opt_ret = Symbol(opt_res.stop_reason)
     stats = Optimization.OptimizationStats(;
         iterations = opt_res.iterations,
