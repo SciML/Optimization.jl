ae29cad0b36e212ebe6acc282f1befdb5f329d37
Alonso Martínez Cisneros <alonso.mtz99@gmail.com>
2024-08-07 16:49:01 +0200

    Implemented OptimizationProblem constructor & initialized project

--- lib/OptimizationNLPModels/src/OptimizationNLPModels.jl
+++ lib/OptimizationNLPModels/src/OptimizationNLPModels.jl
@@ -21,7 +21,29 @@ function OptimizationFunction(
         hess_prototype, cons_jac_prototype, lag_h, kwargs...)
 end
 
-function OptimizationProblem(nlpmodel::NLPModel{T, S})
+function OptimizationProblem(nlpmodel::NLPModel, adtype::AbstractADType = NoAD(); kwargs...)
+    f = OptimizationFunction(nlpmodel, adtype, kwargs...)
+    # FIXME: Check lengths
+    u0 = nlp.meta.x0
+    lb = nlp.meta.lvar
+    ub = nlp.meta.uvar
+    lcons = nlp.meta.lcon
+    ucons = nlp.meta.ucon
+    sense = nlp.meta.minimize ? Optimization.MinSense() : Optimization.MaxSense()
+
+    n = length(u0)
+    @assert n == nlp.meta.nvar
+    err_intro = "Error converting `NLPModel: $(nlpmodel) "
+
+    length(lb) != n && error(err_intro * "More lower bounds than variables were given")
+    length(ub) != n && error(err_intro * "More upper bounds than variables were given")
+    length(lcons) != n &&
+        error(err_intro * "More inequality lower bounds than variables were given")
+    length(ucons) != n &&
+        error(err_intro * "More inequality upper bounds than variables were given")
+
+    return OptimizationProblem(
+        f, u0, lb, ub, lcons, ucons, sense; prob.kwargs..., kwargs...)
 end
 
 end
