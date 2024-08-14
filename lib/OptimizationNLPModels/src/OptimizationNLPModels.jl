074e5335eec8e2e13a5297ed7e14a7b81122cf18
Alonso Martínez Cisneros <alonso.mtz99@gmail.com>
2024-08-14 17:14:53 +0200

    Re-introduced adtype kwarg & fixed passing of constraints for unconstrained probs

--- lib/OptimizationNLPModels/src/OptimizationNLPModels.jl
+++ lib/OptimizationNLPModels/src/OptimizationNLPModels.jl
@@ -3,23 +3,28 @@ module OptimizationNLPModels
 using Reexport
 @reexport using NLPModels, Optimization, ADTypes
 
-function SciMLBase.OptimizationFunction(
-        nlpmodel::AbstractNLPModel; kwargs...)
+function SciMLBase.OptimizationFunction(nlpmodel::AbstractNLPModel,
+        adtype::ADTypes.AbstractADType = SciMLBase.NoAD(); kwargs...)
     f(x, p) = NLPModels.obj(nlpmodel, x)
     grad(G, u, p) = NLPModels.grad!(nlpmodel, u, G)
-    hess(u, p) = NLPModels.hess(nlpmodel, u)
+    hess(H, u, p) = (H .= NLPModels.hess(nlpmodel, u))
     hv(Hv, u, v, p) = NLPModels.hprod!(nlpmodel, u, v, Hv)
-    cons(res, x, p) = NLPModels.cons!(nlpmodel, x, res)
-    cons_j(x, p) = NLPModels.jac(nlpmodel, x)
-    cons_jvp(Jv, v, x, p) = NLPModels.jprod!(nlpmodel, x, v, Jv)
-    lag_h(x, sigma, mu, p) = NLPModels.hess(nlpmodel, x, mu; obj_weight = sigma)
 
-    return OptimizationFunction(f; grad, hess, hv, cons, cons_j, cons_jvp,
-        lag_h, kwargs...)
+    if !unconstrained(nlpmodel)
+        cons(res, x, p) = NLPModels.cons!(nlpmodel, x, res)
+        cons_j(J, x, p) = (J .= NLPModels.jac(nlpmodel, x))
+        cons_jvp(Jv, v, x, p) = NLPModels.jprod!(nlpmodel, x, v, Jv)
+        lag_h(x, sigma, mu, p) = NLPModels.hess(nlpmodel, x, mu; obj_weight = sigma)
+
+        return OptimizationFunction(
+            f, adtype; grad, hess, hv, cons, cons_j, cons_jvp, lag_h, kwargs...)
+    end
+
+    return OptimizationFunction(f, adtype; grad, hess, hv, kwargs...)
 end
 
-function OptimizationProblem(
-        nlpmodel::AbstractNLPModel; kwargs...)
+function OptimizationProblem(nlpmodel::AbstractNLPModel,
+        adtype::ADTypes.AbstractADType = SciMLBase.NoAD(); kwargs...)
     f = OptimizationFunction(nlpmodel, kwargs...)
     u0 = nlpmodel.meta.x0
     lb = nlpmodel.meta.lvar
@@ -32,7 +37,7 @@ function OptimizationProblem(
     # nlpmodel was created.
 
     return Optimization.OptimizationProblem(
-        f, u0; lb=lb, ub=ub, lcons=lcons, ucons=ucons, sense=sense, kwargs...)
+        f, u0; lb = lb, ub = ub, lcons = lcons, ucons = ucons, sense = sense, kwargs...)
 end
 
 end
