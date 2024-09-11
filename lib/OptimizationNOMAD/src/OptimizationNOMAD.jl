7e763ecaee03ceba96e8d9148cb907244c1db46c
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2024-09-11 08:39:11 -0400

    Add constraints support for NOMAD

--- lib/OptimizationNOMAD/src/OptimizationNOMAD.jl
+++ lib/OptimizationNOMAD/src/OptimizationNOMAD.jl
@@ -7,8 +7,11 @@ using NOMAD, Optimization.SciMLBase
 export NOMADOpt
 struct NOMADOpt end
 
+@enum ConstraintBarrierType ExtremeBarrierMethod ProgressiveBarrierMethod
+
 SciMLBase.allowsbounds(::NOMADOpt) = true
 SciMLBase.allowscallback(::NOMADOpt) = false
+SciMLBase.allowsconstraints(::NOMADOpt) = true
 
 function __map_optimizer_args!(prob::OptimizationProblem, opt::NOMAD.NomadProblem;
         callback = nothing,
@@ -40,12 +43,14 @@ function __map_optimizer_args!(prob::OptimizationProblem, opt::NOMAD.NomadProble
     return nothing
 end
 
+@inline strcnsmethod(m::ConstraintBarrierType) = m === ExtremeBarrierMethod ? "EB" : "PB"
+
 function SciMLBase.__solve(prob::OptimizationProblem, opt::NOMADOpt;
         maxiters::Union{Number, Nothing} = nothing,
         maxtime::Union{Number, Nothing} = nothing,
         abstol::Union{Number, Nothing} = nothing,
         reltol::Union{Number, Nothing} = nothing,
-        progress = false,
+        cons_method = ExtremeBarrierMethod,
         kwargs...)
     local x
 
@@ -57,15 +62,27 @@ function SciMLBase.__solve(prob::OptimizationProblem, opt::NOMADOpt;
         return first(x)
     end
 
-    function bb(x)
-        l = _loss(x)
-        success = !isnan(l) && !isinf(l)
-        count_eval = true
-        return (success, count_eval, [l])
-    end
-
-    if !isnothing(prob.lcons) | !isnothing(prob.ucons)
-        @warn "Linear and nonlinear constraints defined in OptimizationProblem are currently not used by $(opt)"
+    if prob.f.cons === nothing
+        function bb(x)
+            l = _loss(x)
+            success = !isnan(l) && !isinf(l)
+            count_eval = true
+            return (success, count_eval, [l])
+        end
+    else
+        eqinds = findall(i -> prob.lcons[i] == prob.ucons[i], 1:length(prob.ucons))
+        function bbcons(x)
+            l = _loss(x)
+            c = zeros(eltype(x), length(prob.ucons))
+            prob.f.cons(c, x, prob.p)
+            c -= prob.ucons
+            if !isempty(eqinds)
+                c[eqinds] = abs.(c[eqinds])
+            end
+            success = !isnan(l) && !isinf(l)
+            count_eval = true
+            return (success, count_eval, vcat(l, c))
+        end
     end
 
     bounds = (;)
@@ -77,7 +94,11 @@ function SciMLBase.__solve(prob::OptimizationProblem, opt::NOMADOpt;
         bounds = (; bounds..., upper_bound = prob.ub)
     end
 
-    opt_setup = NOMAD.NomadProblem(length(prob.u0), 1, ["OBJ"], bb; bounds...)
+    if prob.f.cons === nothing
+        opt_setup = NOMAD.NomadProblem(length(prob.u0), 1, ["OBJ"], bb; bounds...)
+    else
+        opt_setup = NOMAD.NomadProblem(length(prob.u0), 1+length(prob.ucons), vcat("OBJ", fill(strcnsmethod(cons_method), length(prob.ucons))), bbcons; bounds...)
+    end
 
     __map_optimizer_args!(prob, opt_setup, maxiters = maxiters, maxtime = maxtime,
         abstol = abstol, reltol = reltol; kwargs...)
