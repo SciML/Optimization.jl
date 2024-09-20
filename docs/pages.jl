ea583733ba3d527840f516665ed4a6cf0fd8d012
Vaibhav Kumar Dixit <vaibhavyashdixit@gmail.com>
2024-09-20 11:42:30 -0400

    Update pages.jl

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,10 +1,13 @@
 pages = ["index.md",
     "getting_started.md",
     "Tutorials" => [
-        "tutorials/minibatch.md",
-        "tutorials/symbolic.md",
+        "tutorials/certification.md",
         "tutorials/constraints.md",
-        "tutorials/linearandinteger.md"
+        "tutorials/ensemble.md",
+        "tutorials/linearandinteger.md",
+        "tutorials/minibatch.md",
+        "tutorials/remakecomposition.md",
+        "tutorials/symbolic.md",
     ],
     "Examples" => [
         "examples/rosenbrock.md"
@@ -30,13 +33,14 @@ pages = ["index.md",
         "Metaheuristics.jl" => "optimization_packages/metaheuristics.md",
         "MultistartOptimization.jl" => "optimization_packages/multistartoptimization.md",
         "NLopt.jl" => "optimization_packages/nlopt.md",
+        "NLPModels.jl" => "optimization_packages/nlpmodels.md"
         "NOMAD.jl" => "optimization_packages/nomad.md",
         "Optim.jl" => "optimization_packages/optim.md",
         "Optimisers.jl" => "optimization_packages/optimisers.md",
-        "PRIMA.jl" => "optimization_packages/prima.md",
+        "Optimization.jl" => "optimization_packages/optimization.md",
         "Polyalgorithms.jl" => "optimization_packages/polyopt.md",
+        "PRIMA.jl" => "optimization_packages/prima.md",
         "QuadDIRECT.jl" => "optimization_packages/quaddirect.md",
         "SpeedMapping.jl" => "optimization_packages/speedmapping.md",
-        "NLPModels.jl" => "optimization_packages/nlpmodels.md"
     ]
 ]
