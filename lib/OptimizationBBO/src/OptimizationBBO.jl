d8fea39c687e8fac083290207f78308109030eda
Paras Puneet Singh <136245940+ParasPuneetSingh@users.noreply.github.com>
2025-01-30 00:05:48 +0530

    Update OptimizationBBO.jl
    Adding argument mapping for num_dimensions and fitness_scheme.

--- lib/OptimizationBBO/src/OptimizationBBO.jl
+++ lib/OptimizationBBO/src/OptimizationBBO.jl
@@ -49,6 +49,8 @@ function __map_optimizer_args(prob::Optimization.OptimizationCache, opt::BBO;
         abstol::Union{Number, Nothing} = nothing,
         reltol::Union{Number, Nothing} = nothing,
         verbose::Bool = false,
+        num_dimensions::Union{Number, Nothing} = nothing,
+        fitness_scheme::Union{String, Nothing} = nothing,
         kwargs...)
     if !isnothing(reltol)
         @warn "common reltol is currently not used by $(opt)"
@@ -80,6 +82,16 @@ function __map_optimizer_args(prob::Optimization.OptimizationCache, opt::BBO;
         mapped_args = (; mapped_args..., TraceMode = :silent)
     end
 
+    if isa(prob.f, MultiObjectiveOptimizationFunction)
+        if isnothing(num_dimensions) && isnothing(fitness_scheme)
+            mapped_args = (; mapped_args..., NumDimensions = 2, FitnessScheme = BlackBoxOptim.ParetoFitnessScheme{2}(is_minimizing=true))
+        elseif isnothing(num_dimensions)
+            mapped_args = (; mapped_args..., NumDimensions = 2, FitnessScheme = fitness_scheme)
+        elseif isnothing(fitness_scheme)
+            mapped_args = (; mapped_args..., NumDimensions = num_dimensions, FitnessScheme = BlackBoxOptim.ParetoFitnessScheme{2}(is_minimizing=true))
+        end
+    end
+    
     return mapped_args
 end
 
@@ -177,13 +189,25 @@ function SciMLBase.__solve(cache::Optimization.OptimizationCache{
         end
     end
 
-    opt_args = __map_optimizer_args(cache, cache.opt;
-        callback = cache.callback === Optimization.DEFAULT_CALLBACK &&
-                   cache.data === Optimization.DEFAULT_DATA ?
-                   nothing : _cb,
-        cache.solver_args...,
-        maxiters = maxiters,
-        maxtime = maxtime)
+    if isa(cache.f, MultiObjectiveOptimizationFunction)
+        opt_args = __map_optimizer_args(cache, cache.opt;
+            callback = cache.callback === Optimization.DEFAULT_CALLBACK &&
+                    cache.data === Optimization.DEFAULT_DATA ?
+                    nothing : _cb,
+            cache.solver_args...,
+            maxiters = maxiters,
+            maxtime = maxtime,
+            num_dimensions = isnothing(cache.num_dimensions) ? nothing : cache.num_dimensions,
+            fitness_scheme = isnothing(cache.fitness_scheme) ? nothing : cache.fitness_scheme)
+    else
+        opt_args = __map_optimizer_args(cache, cache.opt;
+            callback = cache.callback === Optimization.DEFAULT_CALLBACK &&
+                    cache.data === Optimization.DEFAULT_DATA ?
+                    nothing : _cb,
+            cache.solver_args...,
+            maxiters = maxiters,
+            maxtime = maxtime)
+    end
 
     opt_setup = BlackBoxOptim.bbsetup(_loss; opt_args...)
 
