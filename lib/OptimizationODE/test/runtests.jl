9e03221e1a60508bcb7cea8efdf0d39bc76028ff
Paras Puneet Singh <136245940+ParasPuneetSingh@users.noreply.github.com>
2025-05-31 01:27:01 +0530

    Update runtests.jl
    Redundant parameters removed, HighOrderDescent name update.

--- lib/OptimizationODE/test/runtests.jl
+++ lib/OptimizationODE/test/runtests.jl
@@ -20,8 +20,8 @@ using LinearAlgebra
     f_autodiff = OptimizationFunction(f, ADTypes.AutoForwardDiff())
     prob_auto = OptimizationProblem(f_autodiff, x0, p)
 
-    for opt in (ODEGradientDescent(), RKChebyshevDescent(), RKAccelerated(), PRKChebyshevDescent())
-        sol = solve(prob_auto, opt; η=0.01,dt=0.01, tmax=1000, maxiters=50_000)
+    for opt in (ODEGradientDescent(), RKChebyshevDescent(), RKAccelerated(), HighOrderDescent())
+        sol = solve(prob_auto, opt; dt=0.01, maxiters=50_000)
         @test sol.u ≈ [0.0, 0.0] atol=1e-2
         @test sol.objective ≈ 0.0 atol=1e-2
         @test sol.retcode == ReturnCode.Success
@@ -30,8 +30,8 @@ using LinearAlgebra
     f_manual = OptimizationFunction(f, SciMLBase.NoAD(); grad=g!)
     prob_manual = OptimizationProblem(f_manual, x0)
 
-    for opt in (ODEGradientDescent(), RKChebyshevDescent(), RKAccelerated(), PRKChebyshevDescent())
-        sol = solve(prob_manual, opt; η=0.01,dt=0.01, tmax=1000, maxiters=50_000)
+    for opt in (ODEGradientDescent(), RKChebyshevDescent(), RKAccelerated(), HighOrderDescent())
+        sol = solve(prob_manual, opt; dt=0.01, maxiters=50_000)
         @test sol.u ≈ [0.0, 0.0] atol=1e-2
         @test sol.objective ≈ 0.0 atol=1e-2
         @test sol.retcode == ReturnCode.Success
@@ -40,8 +40,8 @@ using LinearAlgebra
     f_fail = OptimizationFunction(f, SciMLBase.NoAD())
     prob_fail = OptimizationProblem(f_fail, x0)
 
-    for opt in (ODEGradientDescent(), RKChebyshevDescent(), RKAccelerated(), PRKChebyshevDescent())
-        @test_throws ErrorException solve(prob_fail, opt; η=0.01,dt=0.001, tmax=10_000.0, maxiters=20_000)
+    for opt in (ODEGradientDescent(), RKChebyshevDescent(), RKAccelerated(), HighOrderDescent())
+        @test_throws ErrorException solve(prob_fail, opt; dt=0.001, maxiters=20_000)
     end
 
 end
