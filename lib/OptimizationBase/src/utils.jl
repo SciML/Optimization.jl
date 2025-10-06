71aaec9871a9276eb7b9983888f90c807cfa31cf
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-10-06 04:00:16 -0400

    Update utils.jl

--- lib/OptimizationBase/src/utils.jl
+++ lib/OptimizationBase/src/utils.jl
@@ -4,36 +4,6 @@ function get_maxiters(data)
     typemax(Int) : length(data)
 end
 
-maybe_with_logger(f, logger) = logger === nothing ? f() : Logging.with_logger(f, logger)
-
-function default_logger(logger)
-    Logging.min_enabled_level(logger) ≤ ProgressLogging.ProgressLevel && return nothing
-    if Sys.iswindows() || (isdefined(Main, :IJulia) && Main.IJulia.inited)
-        progresslogger = ConsoleProgressMonitor.ProgressLogger()
-    else
-        progresslogger = TerminalLoggers.TerminalLogger()
-    end
-    logger1 = LoggingExtras.EarlyFilteredLogger(progresslogger) do log
-        log.level == ProgressLogging.ProgressLevel
-    end
-    logger2 = LoggingExtras.EarlyFilteredLogger(logger) do log
-        log.level != ProgressLogging.ProgressLevel
-    end
-    LoggingExtras.TeeLogger(logger1, logger2)
-end
-
-macro withprogress(progress, exprs...)
-    quote
-        if $progress
-            $maybe_with_logger($default_logger($Logging.current_logger())) do
-                $ProgressLogging.@withprogress $(exprs...)
-            end
-        else
-            $(exprs[end])
-        end
-    end |> esc
-end
-
 decompose_trace(trace) = trace
 
 function _check_and_convert_maxiters(maxiters)
