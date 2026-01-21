3be49bf4764b0250971c24dd49e05b2544eb36fe
jClugstor <jadonclugston@gmail.com>
2026-01-21 13:37:12 -0500

    add Mooncake rules that call SciMLSensitivity

---
+++ lib/OptimizationBase/ext/OptimizationMooncakeExt.jl
@@ -0,0 +1,18 @@
+module OptimizationMooncakeExt
+
+using OptimizationBase, Mooncake
+using SciMLBase
+using Mooncake: rrule!!, CoDual, zero_fcodual, @is_primitive, 
+    @from_chainrules, @zero_adjoint, @mooncake_overlay, MinimalCtx,
+    NoPullback
+
+@from_chainrules MinimalCtx Tuple{
+    typeof(OptimizationBase.solve_up),
+    SciMLBase.AbstractOptimizationProblem,
+    Union{Nothing, SciMLBase.AbstractSensitivityAlgorithm},
+    Any,
+    Any,
+    Any,
+} true
+
+end
