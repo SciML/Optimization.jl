9ce6c05724de960387865d441375eb69163524d0
jClugstor <jadonclugston@gmail.com>
2026-01-21 13:35:56 -0500

    add Enzyme rules that call SciMLSensitivity

--- lib/OptimizationBase/ext/OptimizationEnzymeExt.jl
+++ lib/OptimizationBase/ext/OptimizationEnzymeExt.jl
@@ -808,4 +808,62 @@ function OptimizationBase.instantiate_function(
     return OptimizationBase.instantiate_function(f, x, adtype, p, num_cons; kwargs...)
 end
 
+function Enzyme.EnzymeRules.augmented_primal(
+    config::Enzyme.EnzymeRules.RevConfigWidth{1},
+    func::Const{typeof(OptimizationBase.solve_up)}, ::Type{Duplicated{RT}}, prob,
+    sensealg::Unions{
+        Const{Nothing}, Const{<:SciMLBase.AbstractSensitivityAlgorithm}
+    },
+    u0, p, args...; kwargs...
+) where {RT}
+
+    @inline function copy_or_reuse(val, idx)
+        if Enzyme.EnzymeRules.overwritten(config)[idx] && ismutable(val)
+            return deepcopy(val)
+        else
+            return val
+        end
+    end
+
+    @inline function arg_copy(i)
+        return copy_or_reuse(args[i].val, i + 5)
+    end
+
+    res = OptimizationBase._solve_adjoint(
+        copy_or_reuse(prob.val, 2), copy_or_reuse(sensealg.val, 3),
+        copy_or_reuse(u0.val, 4), copy_or_reuse(p.val, 5),
+        SciMLBase.EnzymeOriginator(), ntuple(arg_copy, Val(length(args)))...;
+        kwargs...
+    )
+
+    dres = Enzyme.make_zero(res[1])::RT
+    tup = (dres, res[2])
+    return Enzyme.EnzymeRules.AugmentedReturn{RT,RT,Any}(res[1], dres, tup::Any)
+end
+
+function Enzyme.EnzymeRules.reverse(
+    config::Enzyme.EnzymeRules.RevConfigWidth{1},
+    func::Const{typeof(OptimizationBase.solve_up)}, ::Type{Duplicated{RT}}, tape, prob, 
+    sensealg::Union{
+        Const{Nothing}, Const{<:SciMLBase.AbstractSensitivityAlgorithm},
+    },
+    u0, p, args...; kwargs...
+    ) where {RT}
+    dres, clos = tape
+    dres = dres::RT
+    dargs = clos(dres)
+
+    for (darg, ptr) in zip(dargs, (fun, prob, sensealg, u0, p, args...))
+        if ptr isa Enzyme.Const
+            continue
+        end
+        if darg == ChainRulesCore.NoTangent()
+            continue
+        end
+        ptr.dval .+= darg 
+    end
+    Enzyme.make_zero!(dres.u)
+    return ntuple(_->nothing, Val(length(args) + 4))
+end
+
 end
